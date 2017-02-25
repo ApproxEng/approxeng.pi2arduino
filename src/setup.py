4dcc62bd10100d21b35314b02b59286021b6ea66
tom <tomoinn@gmail.com>
2017-02-25 00:19:19 +0000

    Added better specification for setup

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,21 @@
 from distutils.core import setup, Extension
 
-extension_mod = Extension("approxeng.pi2arduino._p2a", sources=["linux/_p2a.c", "linux/p2a.c", "linux/pigpio.c", "linux/command.c"],
-                          extra_compile_args=['-Wno-strict-prototypes', '-Wno-error=format-security'])
+extension_mod = Extension("approxeng.pi2arduino._p2a",
+                          sources=["linux/_p2a.c",
+                                   "linux/p2a.c",
+                                   "linux/pigpio.c",
+                                   "linux/command.c"],
+                          extra_compile_args=['-Wno-strict-prototypes',
+                                              '-Wno-error=format-security'])
 
-setup(name="p2a", ext_modules=[extension_mod])
+setup(name="approxeng.pi2arduino",
+      version='0.1',
+      description='Python support for Pi to Arduino bi-directional communication over I2C',
+      classifiers=['Programming Language :: Python :: 2.7'],
+      url='https://github.com/ApproxEng/approxeng.pi2arduino/',
+      author='Tom Oinn',
+      author_email='tomoinn@gmail.com',
+      license='ASL2.0',
+      packages=['approxeng.pi2arduino'],
+      zip_safe=False,
+      ext_modules=[extension_mod])
