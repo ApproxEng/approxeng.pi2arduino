d036cd7c3fe26447d8e8050c1ccde7968b58b38b
Tom <tomoinn@gmail.com>
2017-02-25 00:08:13 +0000

    moved native library to package

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-extension_mod = Extension("_p2a", ["_p2a.c", "p2a.c", "pigpio.c", "command.c"],
+extension_mod = Extension("approxeng.pi2arduino._p2a", sources=["linux/_p2a.c", "linux/p2a.c", "linux/pigpio.c", "linux/command.c"],
                           extra_compile_args=['-Wno-strict-prototypes', '-Wno-error=format-security'])
 
 setup(name="p2a", ext_modules=[extension_mod])
