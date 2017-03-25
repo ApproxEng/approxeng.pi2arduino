9d109a2670ea035d9d3830a444d1c90fa4d1b68b
tom <tomoinn@gmail.com>
2017-03-25 23:45:02 +0000

    Config tweaks, added test read() method
    Signed-off-by: tom <tomoinn@gmail.com>

--- src/approxeng/pi2arduino/__init__.py
+++ src/approxeng/pi2arduino/__init__.py
@@ -1,24 +1,13 @@
 import struct
 
-# noinspection PyUnresolvedReferences
-import approxeng.pi2arduino._p2a as p2a_native
+import approxeng.pi2arduino._p2a
 
 
 def send(address, *sequence):
-    packed = ''
+    packed = ""
     for item in sequence:
         if isinstance(item, int) and 0 <= item <= 255:
-            packed += struct.pack('B', item)
+            packed += struct.pack("B", item)
         elif isinstance(item, float):
-            packed += struct.pack('f', item)
-    p2a_native.sendBytes(address, packed)
-
-
-def read(address, format_string):
-    length = 0
-    for c in format_string:
-        if c == 'B':
-            length += 1
-        elif c == 'f':
-            length += 4
-    print p2a_native.getBytes(address, length)
+            packed += struct.pack("f", item)
+    approxeng.pi2arduino._p2a.sendBytes(address, packed)
