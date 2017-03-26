81f1feb3396f3d3f5765f06b27036b51f74e8e33
tom <tomoinn@gmail.com>
2017-03-26 00:49:13 +0000

    Added read method
    Signed-off-by: tom <tomoinn@gmail.com>

--- src/approxeng/pi2arduino/__init__.py
+++ src/approxeng/pi2arduino/__init__.py
@@ -4,10 +4,28 @@ import approxeng.pi2arduino._p2a
 
 
 class I2CHelper(object):
+    """
+    Class to handle communication via the p2a.c native code. Packages and unpackages sequences of
+    numeric data and initialises the I2C device on construction.
+    """
+
     def __init__(self):
+        """
+        Initialise - this will require root access after which it can be dropped.
+        """
+
         approxeng.pi2arduino._p2a.init()
 
     def send(self, address, *sequence):
+        """
+        Send a message consisting of a sequence of integers (between 0 and 255) and floats to 
+        a device on the I2C bus
+        
+        :param address: 
+            The I2C address of the receiver
+        :param sequence: 
+            An arbitrary number of ints and floats to send
+        """
         packed = ''
         for item in sequence:
             if isinstance(item, int) and 0 <= item <= 255:
@@ -17,6 +35,18 @@ class I2CHelper(object):
         approxeng.pi2arduino._p2a.sendBytes(address, packed)
 
     def read(self, address, format_string):
+        """
+        Read data from a device on the I2C bus
+        
+        :param address: 
+            The I2C address of the node to query
+        :param format_string: 
+            A format string, currently only supporting 'f' for floats and 'B' for byte values, this
+            is used both to determine the size of the request, and then to unpack the request into
+            a sequence of numeric values of the specified type or types
+        :return: 
+            A sequence of numeric data from the I2C device
+        """
         length = 0
         for token in format_string:
             if token == 'B':
@@ -24,4 +54,4 @@ class I2CHelper(object):
             elif token == 'f':
                 length += 4
         data = approxeng.pi2arduino._p2a.getBytes(address, length)
-        return data
+        return struct.unpack(format_string, data)
